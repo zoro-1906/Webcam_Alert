1f49ff84e7705f1e0f180a830e635dc186057e58
Zuhair <rawootzuhairahmed@gmial.com>
2024-10-01 18:47:44 +0530

    fixed a bug

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ while True:
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
+
     if first_frame is None:
         first_frame = gray_frame_gau
 
