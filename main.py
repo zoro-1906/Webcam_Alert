6c7d18ddb4c8a02a77a83f57c902d90d68ba6084
Zuhair <rawootzuhairahmed@gmial.com>
2024-10-01 18:40:29 +0530

    detecting a new obj in cam

---
+++ main.py
@@ -0,0 +1,37 @@
+import cv2
+import time
+
+video = cv2.VideoCapture(0)
+time.sleep(1)
+first_frame = None
+
+while True:
+    check, frame = video.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+
+    thresh_frame = cv2.threshold(delta_frame, 100, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    cv2.imshow("My video", dil_frame)
+
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+    cv2.imshow("video", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+video.release()
